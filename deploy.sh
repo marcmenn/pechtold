dc1485300258d1c58a58761cf05624ca6af65417
Marcus Mennemeier <m.mennemeier@global-communication.de>
2014-05-17 17:44:02 +0200

    clearing output before deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 npm install
+rm -rf out/
 node_modules/.bin/docpad generate --env static
 rsync -rvzp --size-only --delete out/ upload@dev.global-communication.de:/home/pechtold.global-communication.de/www/
